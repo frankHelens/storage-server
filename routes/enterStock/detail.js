5414379a62f22211a19313ffba292be544474c53
黄丰瑞 <798356548@qq.com>
2018-03-07 22:06:59 +0800

    入库提交事务

--- routes/enterStock/detail.js
+++ routes/enterStock/detail.js
@@ -7,36 +7,37 @@ const router = express.Router()
 
 router.route('/')
   .get((req, res) => {
-    const test = {
-      base: {
-        createdAt: 1511704749000,
-        origin: 'asdfadfall',
-        storageType: 'storage',
-        enterPrice: 500.00,
-        takePe: '黄小大',
-        makePe: '黄大小',
-        remark: '我是备注'
-      },
-      tableData: [{
-        productId: 100,
-        enterNum: 200,
-        discount: 100,
-        remark: '这个是单个的商品价格1'
-      }, {
-        productId: 200,
-        enterNum: 2,
-        discount: 100,
-        remark: '这个是单个的商品价格2'
-      }, {
-        productId: 300,
-        enterNum: 3,
-        discount: 100,
-        remark: '这个是单个的商品价格3'
-      }]
-    }
-    enterStoreCreate(test)
+    // const test = {
+    //   base: {
+    //     createdAt: 1511704749000,
+    //     origin: 'asdfadfall',
+    //     storageType: 'storage',
+    //     enterPrice: 500.00,
+    //     takePe: '黄小大',
+    //     makePe: '黄大小',
+    //     remark: '我是备注'
+    //   },
+    //   tableData: [{
+    //     productId: 100,
+    //     enterNum: 200,
+    //     discount: 100,
+    //     remark: '这个是单个的商品价格1'
+    //   }, {
+    //     productId: 200,
+    //     enterNum: 2,
+    //     discount: 100,
+    //     remark: '这个是单个的商品价格2'
+    //   }, {
+    //     productId: 300,
+    //     enterNum: 3,
+    //     discount: 100,
+    //     remark: '这个是单个的商品价格3'
+    //   }]
+    // }
+  })
+  .post((req, res) => {
+    enterStoreCreate(req.body)
     .then((data) => {
-      console.log('出来吧：', data)
       res.send(data)
     })
   })
