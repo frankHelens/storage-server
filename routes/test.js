7addebdf9f8f6f386a451111df8e6a58e6185110
frankHelens <798356548@qq.com>
2018-03-19 08:54:04 +0800

    测试用例

--- routes/test.js
+++ routes/test.js
@@ -1,31 +1,31 @@
 import express from 'express'
-
+import sequelize from '../DB/config'
 import { EnterStock, EnterStockDetail, Product } from '../Model'
 import { enterStoreCreate, getEnterStoreDetail } from '../Controller/enterStore';
 
 const router = express.Router();
 
+const arr = [6, 7, 8, 9]
+
 router.route('/')
 .get((req, res) => {
-  EnterStock.findAll({
-    include: {
-      model: EnterStockDetail
-    },
-    where: {
-      id: 1
-    }
+  return sequelize.transaction((t) => {
+    return arr.map(item => {
+      return Product.update({
+        productNum: 30
+      }, {
+        where: {
+          id: item
+        }
+      }, {transaction: t})
+    })
   })
-  .then((data) => {
-    const resData = data.filter(item => item.enter_stock_detail)
-    res.send(resData) 
-    // EnterStockDetail.destroy({
-    //   where: {
-    //     id: data.map(item => item.enter_stock_detail.id)
-    //   }
-    // })
-    // .then(data => {
-    //   res.send('删除成功')
-    // })
+  .then((result) => {
+    res.send(result)
+  })
+  .catch((error) => {
+    console.log(error)
+    // res.send(result)
   })
 })
 
