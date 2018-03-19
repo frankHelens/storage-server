d5705332b17eb89cffc3bae6014c678309f31f64
frankHelens <798356548@qq.com>
2018-03-19 08:53:37 +0800

    删除多余代码

--- Model/index.js
+++ Model/index.js
@@ -2,13 +2,9 @@ import Dictionaries from './dictionaries'
 import EnterStock from './enterStock'
 import EnterStockDetail from './enterStockDetail'
 import Product from './product'
-import Sequelize from 'sequelize' // ORM
 
 EnterStockDetail.belongsTo(Product)
 EnterStock.hasOne(EnterStockDetail)
-EnterStock.hasMany(EnterStockDetail)
-
-Sequelize.sync
 
 module.exports = {
   Dictionaries,
