e7ee0a76c34d41c46b2a2e190d3d2231f7a0d4ec
frankHelens <798356548@qq.com>
2018-03-26 09:52:59 +0800

    添加登陆

---
+++ Controller/login.js
@@ -0,0 +1,39 @@
+import { User } from '../Model'
+import { Message, signToken } from '../utils/common'
+
+export const loginCheck = ({body}) => {
+  const { loginName, loginPassword } = body
+  return User.findAll({
+    where: {
+      loginName
+    }
+  })
+  .then((nameRes) => {
+    if (nameRes.length) {
+      return User.findAll({
+        where: {
+          loginName,
+          loginPassword
+        }
+      })
+      .then(res => {
+        if (res.length) {
+          const { id, loginName, name, loginPassword } = res[0]
+          const userMessage = {
+            userId: id,
+            loginName,
+            realname: name,
+            loginPassword
+          }
+          return Message(0, {
+            token: signToken(userMessage)
+          }, '登陆成功')
+        } else{
+          return Message(-1, {}, '密码错误！')
+        }
+      })
+    } else {
+      return Message(-1, {}, '用户不存在!')
+    }
+  })
+}
--- routes/login.js
+++ routes/login.js
@@ -1,26 +1,14 @@
 import express from 'express'
-//引入数据库Message模块
-import Message from '../Model/login'
+import { loginCheck } from '../Controller/login'
 
 const router = express.Router();
 
 router.route('/')
-  .get((req, res) => {
-    //查找所有留言
-    Message.create({  
-      username: 'frank',
-      content: '老杨'
-    });
-    Message.findAll().then(function(msgs) {
-      res.send({ messages: msgs });
-    });
-    // res.send('login');
-  })
-  .post((req, res) => {
-    res.send('post-login');
-  })
   .put((req, res) => {
-    res.send('put-login');
-  });
-  
+    loginCheck(req)
+    .then((data) => {
+      res.send(data)
+    })
+  })
+
 module.exports = router;
